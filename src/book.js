6528185502295171452dfac959ad53ba907088f7
Matt Ruder <matt.j.ruder@gmail.com>
2022-02-03 16:04:58 -0700

    add book test

--- src/book.js
+++ src/book.js
@@ -1,12 +1,45 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return (`The ${title}`);
 }
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+}
+
+
+function saveReview(review, reviews) {
+  if (!reviews.includes(review)) {
+    reviews.push(review);
+  }
+};
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20;
+}
+
+function writeBook(title, mainCharacter, genre) {
+  return {
+    title,
+    mainCharacter,
+    pageCount: calculatePageCount(title),
+    genre
+}
+}
+
+function editBook(title) {
+  return title.pageCount *= 0.75;
+}
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
