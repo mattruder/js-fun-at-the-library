09a62b13ca507b6f15f0708cffeaaad91b91cd80
Matt Ruder <matt.j.ruder@gmail.com>
2022-02-03 17:46:30 -0700

    add shelf test

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,41 @@
+function shelfBook(title, shelf) {
+  if (shelf.length <= 2) {
+  return shelf.unshift(title);
+  }
+}
+
+function unshelfBook(bookTitle, shelf) {
+  for (i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === bookTitle) {
+      return shelf.splice(i, 1)
+    }
+  }
+}
+
+function listTitles(shelf) {
+  var shelfTitles = '';
+  for (i = 0; i < shelf.length; i++) {
+    if (i < (shelf.length -1)) {
+      shelfTitles += shelf[i].title + ", "
+    } else {
+      shelfTitles += shelf[i].title
+    }
+  }
+  return shelfTitles;
+}
+
+function searchShelf(shelf, bookTitle) {
+  var titles = [];
+  for (var i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title);
+  }
+  return titles.includes(bookTitle);
+}
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
